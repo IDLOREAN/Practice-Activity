713fc9f58ac75638476babb4c2c93f16795e6887
IDLOREAN <rodrigo.campos.mar@gmail.com>
2025-06-09 18:10:20 +0100

    Primeiro Commit

---
+++ Practice Activity.js
@@ -0,0 +1,15 @@
+alert("Boas vindas ao nosso Site!");
+let nome = "Lua";
+let idade = 18;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert("Erro! Preencha todos os campos");
+let MensagemDeErro = "Erro! Preencha todos os campos"
+let Nomedousuario = prompt ("Qual seu nome?");
+let idadedousuario = prompt("Qual a sua idade?");
+if(idadedousuario >= idade){
+    alert("Pode tirar a habilitação!");
+}
+else {
+   alert("Não pode tirar habilitação");
+}
